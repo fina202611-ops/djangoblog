728dad2b570cb05a758e8fe34837a3a6d71b0e03
fina202611-ops <fina202611@st.forum.ac.jp>
2026-09-14 14:34:33 +0900

    Add slug and status fields to Post model

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.contrib.auth.models import slugify
 
 class Post(models.Model):
     STATUS_CHOICES = [
@@ -7,14 +7,18 @@ class Post(models.Model):
         ("published", "Published"),
     ]
 
-    status = models.CharField(
-        max_length=10, choices=STATUS_CHOICES, default="published"
+    status = models.CharField( max_length=10, choices=STATUS_CHOICES, default="published"
     )
     title = models.CharField(max_length=200)
     slug = models.SlugField(blank=True, unique=True)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return self.title
