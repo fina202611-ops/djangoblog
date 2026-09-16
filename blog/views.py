f25d1b85c79e6250a977af386fe45ea4193d6271
fina202611-ops <fina202611@st.forum.ac.jp>
2026-09-16 10:39:42 +0900

    Add post_list view with pagination

--- blog/views.py
+++ blog/views.py
@@ -1,15 +1,29 @@
+from django.core.paginator import Paginator
 from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here
+from .models import Post
+
+
 def home(request):
-    return render(request, 'blog/home.html', {'title': 'Home'})
+    return render(request, "blog/home.html", {"title": "Home"})
+
 
 def about(request):
-    return render(request, 'blog/about.html', {'title': 'About'})
+    return render(request, "blog/about.html", {"title": "About"})
+
 
 def contact(request):
-    return render(request, 'blog/contact.html', {'title': 'Contact'})
+    return render(request, "blog/contact.html", {"title": "Contact"})
+
 
 def base(request):
-    return render(request, 'blog/base.html', {'title': 'Base'})
+    return render(request, "blog/base.html", {"title": "Base"})
+
+
+def post_list(request):
+    posts = Post.objects.filter(status="published").order_by("-created_at")
+    paginator = Paginator(posts, 6)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+    return render(request, "blog/post_list.html", {"page_obj": page_obj})
